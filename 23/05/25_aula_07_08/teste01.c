09e5294239fcb80860c7dce68878f5924af2d3ce
WALLACE SILVA LOPES <202503078998@alunos.unifanor.edu.br>
2025-05-23 21:39:18 -0300

    primeiro commit

---
+++ 23/05/25_aula_07_08/teste01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <locale.h>
+int main () {
+setlocale(LC_ALL, "Portuguese_Brazil.1252");
+
+    printf("Somos estudantes do curso de ADS da UniFanor.Queremos estar inseridos no mundo da tecnologia da informação. \n");
+    
+    return 0;
+
+}
